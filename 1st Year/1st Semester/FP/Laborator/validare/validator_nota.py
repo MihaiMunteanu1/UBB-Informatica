1762e66c5d038b121f046ab90d3dc250a48b73da
Mihai Munteanu <163286946+MihaiMunteanu1@users.noreply.github.com>
2024-12-22 18:28:50 +0200

    Delete 1st Year/1st Semester/FP/Laborator directory

--- 1st Year/1st Semester/FP/Laborator/validare/validator_nota.py
+++
@@ -1,8 +0,0 @@
-class ValidatorNota:
-
-    def validare_nota(self,nota):
-        erori=""
-        if 0.0>=nota.get_nota() or nota.get_nota()>10.0:
-            erori+="nota invalida!"
-        if len(erori)>0:
-            raise Exception(erori)
